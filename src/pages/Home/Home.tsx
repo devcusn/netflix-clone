e35b4b479f03009014cebd2480cc9d008c9a407b
devcusn <thisistunc@gmail.com>
2022-06-01 22:07:33 +0300

    package: react-router-dom

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+
+const Home: React.FunctionComponent = () => {
+    return (<React.Fragment>
+        <div className="page">
+            <div className="Header">
+                <div className="logo">
+
+                </div>
+                <div className="menu">
+
+                </div>
+            </div>
+            <div className="section">
+                <div className="title"></div>
+                <div className="middle-title"></div>
+                <div className="small-p">
+                </div>
+                <div className="input-area">
+                    <input /><button>Başlayın</button>
+                </div>
+            </div>
+        </div>
+
+    </React.Fragment>)
+}
+
+
+export default Home;
